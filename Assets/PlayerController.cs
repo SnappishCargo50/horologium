a4a928d3427e89ff4dfb9c5e07b369afb3f8b89c
SludgerFox <rileydamron.business@gmail.com>
2023-06-01 14:33:03 -0500

    Added Character Movement

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 30f;
+    public float maxSpeed = 50f;
+    public float jumpForce = 60f;
+    private bool isGrounded = false;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        /* Horizontal Movement */
+        Vector2 moveDirection = new Vector2(horizontalInput, 0);
+        rb.AddForce(moveDirection * moveSpeed);
+        /* Clamps Character Velocity */
+        if (rb.velocity.magnitude > maxSpeed)  
+        {
+            rb.velocity = Vector2.ClampMagnitude(rb.velocity, maxSpeed);
+        }
+        /* Sprint */
+        if (Input.GetKeyDown(KeyCode.LeftControl)){
+            Debug.Log("Sprinting!");
+            moveSpeed = 30f;
+            return;
+        }
+        /* Checks if Character Grounded and Jumps */
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+            isGrounded = false;
+        }
+        /* Flip Character */
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+}
