c830a1a0285d9522c9ed1ba4dabaf3225cccd80f
SludgerFox <rileydamron.business@gmail.com>
2023-06-17 23:26:32 -0500

    Added Animations and Partial LV2

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,25 +4,34 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 100f;
-    public float maxSpeed = 70f;
+    public float maxSpeed = 40f;
     public float jumpForce = 90f;
     public float bounceForce = 60f;
+    private float timeSinceLastSpriteChange = 0f;
+    public float spriteChangeInterval = 0.4f;
     private bool isGrounded = false;
     public bool menu_Begin = false;
     public bool menu_Quit = false;
     public bool sand_Splash_C = false;
     public bool light_Overlay1_C = false;
+    private bool usingSprite1 = true;
+    public Sprite Run1; // Running Sprite 1
+    public Sprite Run2; // Running Sprite
+    public Sprite Default_Idle; // Default Sprite
+    private SpriteRenderer spriteRenderer;
     private Rigidbody2D rb;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
         /* Set the player's spawn origin */
         transform.position = new Vector3(-65.5999985f, 34.4799995f, -117.510002f);
         moveSpeed = 100f;
-        maxSpeed = 70f;
+        maxSpeed = 40f;
         jumpForce = 90f;
         bounceForce = 60f;
+        spriteRenderer.sprite = Default_Idle;
     }
 
     void Update()
@@ -31,8 +40,9 @@ public class PlayerController : MonoBehaviour
         GameObject sandSplashObject = GameObject.FindWithTag("Sand_Splash");
         GameObject lightOverlay1 = GameObject.FindWithTag("Light-Overlay1");
 
-        float horizontalInput = Input.GetAxis("Horizontal");
+        //Movement And Controls
         /* Horizontal Movement */
+        float horizontalInput = Input.GetAxis("Horizontal");
         Vector2 moveDirection = new Vector2(horizontalInput, 0);
         rb.AddForce(moveDirection * moveSpeed);
         /* Clamps Character Velocity */
@@ -44,17 +54,24 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKey(KeyCode.LeftControl))
         {
             Debug.Log("Sprinting!");
-            Debug.Log("Velocity before: " + rb.velocity +" " + maxSpeed);
-            maxSpeed = 85f;
+            jumpForce = 110f;
+            Debug.Log("Velocity before: " + rb.velocity +" Max speed: " + maxSpeed);
+            maxSpeed = 55f;
+            spriteChangeInterval = 0.2f;
             var speedFactor = (maxSpeed - rb.velocity.magnitude) / maxSpeed;
             rb.AddForce(moveDirection * speedFactor * 5 * Vector2.right, ForceMode2D.Impulse);
-            Debug.Log("Velocity after: " + rb.velocity + " " + maxSpeed);
-            maxSpeed = 85f;
+            Debug.Log("Velocity after: " + rb.velocity + " Max speed: " + maxSpeed);
         }
         else 
         {
-            maxSpeed = 70f;
+            maxSpeed = 40f;
+            jumpForce = 90f;
+            spriteChangeInterval = 0.4f;
         }
+
+
+
+
         /* Menu */
         if(Input.GetKeyDown(KeyCode.Escape))
         {
@@ -70,16 +87,40 @@ public class PlayerController : MonoBehaviour
             rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
             isGrounded = false;
         }
-        /* Flip Character */
-        if (Input.GetKeyDown(KeyCode.A))
-        {
+
+
+        /* Flip Character And Start/End Frame Change */
+        if (Input.GetKey(KeyCode.A)){
             transform.localScale = new Vector3(-1, 1, 1);
         }
-        if (Input.GetKeyDown(KeyCode.D))
-        {
+
+        if (Input.GetKey(KeyCode.D)){
             transform.localScale = new Vector3(1, 1, 1);
         }
-        /* Makes Splash Levels Appear */
+
+        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)){
+            timeSinceLastSpriteChange += Time.deltaTime;
+            if (timeSinceLastSpriteChange >= spriteChangeInterval)
+            {
+                if (usingSprite1)
+                {
+                    spriteRenderer.sprite = Run1;
+                }
+                else
+                {
+                    spriteRenderer.sprite = Run2;
+                }
+                usingSprite1 = !usingSprite1;
+                timeSinceLastSpriteChange = 0f;
+            }
+        }
+        else{
+            timeSinceLastSpriteChange = 0f;
+            spriteRenderer.sprite = Default_Idle;
+        }
+
+
+        // Makes Splash Levels Appear
         if ( sand_Splash_C == true){
             sandSplashObject.transform.localScale = new Vector3(4, 3, 1);
         }
@@ -93,7 +134,8 @@ public class PlayerController : MonoBehaviour
         else{
             lightOverlay1.transform.localScale = new Vector3(0, 0, 0);
         }
-        // Excutes Begining
+
+        // Executes Beginning
         if(Input.GetKeyDown(KeyCode.E) & menu_Begin == true){
             transform.position = new Vector3(-34, -107, 3);
             Debug.Log("Teleporting to LV. 1");
@@ -108,7 +150,7 @@ public class PlayerController : MonoBehaviour
         }
 
        // Snap the position to the nearest pixel
-       transform.position = SnapToPixel(transform.position); 
+       transform.position = SnapToPixel(transform.position);
     }
 
     // A method that snaps a vector to the nearest pixel
